3ca17b4c3bf91cd8916c8f45e13e22657d70068e
swapniljavaworld <55404439+swapniljavaworld@users.noreply.github.com>
2019-09-16 22:00:15 +0530

    Two Classes Added
    Two classes of RestTemplate are added

---
+++ RestTemplateClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.Google.RestTemplateClient_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateClientAppApplication.class, args);
+	}
+
+}
